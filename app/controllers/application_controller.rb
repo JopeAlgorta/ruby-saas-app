b0c19f6abfd0a47940704dd774752a56fa9e91f3
Jose Algorta <jpalgort@gmail.com>
2022-02-12 00:41:50 -0300

    Add Device and ActsAsTenant gems for user auth and multi-tenancy.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  set_current_tenant_through_filter
+  before_action
+
+  def set_current_account
+    return unless current_user.present?
+    current_account = current_user.account
+    ActsAsTenant.current_tenant = current_account
+  end
 end
